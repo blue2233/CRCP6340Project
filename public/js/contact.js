18469d20249f9646796890a18c9eff22dfa86ec0
blue2233 <nnurge@gmail.com>
2026-05-22 14:51:56 -0500

    Added Contact-form field validation

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,18 +1,33 @@
 (function () {
-    "use strict";
+  "use strict";
 
-    document
+  let form = document.querySelector('#contact-form');
+
+  document
     .querySelector("#contact-form-button")
     .addEventListener("click", (event) => {
-        event.preventDefault();
-        event.stopPropagation();
-        console.log("You clicked the submit button");
-        let name = document.querySelector("#name").value;
-        let email = document.querySelector("#email").value;
-        let message = document.querySelector("#message").value;
-        console.log("Name: " + name);
-        console.log("Email: " + email);
-        console.log("Message: " + message);
-    });
+    event.preventDefault();
+    event.stopPropagation();
+    let formValid = true;
+    if (!form.checkValidity()) {
+      formValid = false;
+    }
+    form.classList.add('was-validated');
+    if (formValid) {
+      sendTheEmail();
+    }
+  });
 
-}) ();
+function sendTheEmail() {
+  console.log("You clicked the submit button.");
+  let firstName = document.querySelector("#firstName").value;
+  let lastName = document.querySelector("#lastName").value;
+  let email = document.querySelector("#email").value;
+  let message = document.querySelector("#message").value;
+  console.log("First Name: " + firstName);
+  console.log("Last Name: " + lastName);
+  console.log("Email: " + email);
+  console.log("Message: " + message);
+}
+
+})();
