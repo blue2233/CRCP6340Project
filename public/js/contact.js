fb122f4d2a2b3b3f4c442351b3a4c617a47d5a98
blue2233 <nnurge@gmail.com>
2026-05-23 09:35:59 -0500

    Setup nodemailer for the 'Submit Form' functionality on Contact page setup Sendgrid for SMTP relay service configured domain @nordak.io

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,33 +1,49 @@
 (function () {
-  "use strict";
+    "use strict";
 
-  let form = document.querySelector('#contact-form');
+    let form = document.querySelector('#contact-form');
 
-  document
-    .querySelector("#contact-form-button")
-    .addEventListener("click", (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-    let formValid = true;
-    if (!form.checkValidity()) {
-      formValid = false;
+    document
+        .querySelector("#contact-form-button")
+        .addEventListener("click", (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            let formValid = true;
+            if (!form.checkValidity()) {
+                formValid = false;
+            }
+            form.classList.add('was-validated');
+            if (formValid) {
+                sendTheEmail();
+            }
+        });
+
+    function sendTheEmail() {
+        console.log("You clicked the submit button.");
+        let obj = {
+            sub: "Someone contacted you from your website!",
+            txt: `${document.querySelector("#firstName").value}
+            ${document.querySelector("#lastName").value}
+            sent you a message that read ${document.querySelector("#message").value}.
+            Their email address is ${document.querySelector("#email").value
+                }.`
+        };
+        fetch("/mail", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(obj),
+        })
+        .then((r) => r.json())
+        .then((response) => {
+            document.querySelector("#contact-button-response").innerHTML = response.result;
+        })
+        .then(() => {
+            setTimeout(() => {
+                document.querySelector("#contact-button-response").innerHTML = "";
+            }, "5000");
+        });
     }
-    form.classList.add('was-validated');
-    if (formValid) {
-      sendTheEmail();
-    }
-  });
-
-function sendTheEmail() {
-  console.log("You clicked the submit button.");
-  let firstName = document.querySelector("#firstName").value;
-  let lastName = document.querySelector("#lastName").value;
-  let email = document.querySelector("#email").value;
-  let message = document.querySelector("#message").value;
-  console.log("First Name: " + firstName);
-  console.log("Last Name: " + lastName);
-  console.log("Email: " + email);
-  console.log("Message: " + message);
-}
 
 })();
---
+++ utils/utils.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+
+export async function sendMessage(sub, txt) {
+    let transporter = nodemailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        secure: process.env.MAIL_SECURE === 'true',
+        auth: {
+            user: process.env.MAIL_USERNAME,
+            pass: process.env.MAIL_PASSWORD,
+        },
+        requireTLS: process.env.MAIL_TLS === 'true',
+    });
+    let message = {
+        from: process.env.MESSAGE_FROM,
+        to: process.env.MESSAGE_TO,
+        subject: sub,
+        text: txt,
+    };
+    await transporter
+        .sendMail(message)
+        .then(() => {
+            console.log("Message sent");
+        })
+        .catch((err) => {
+            console.log("Message not sent - " + err);
+        });
+}
