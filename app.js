1bf2651a0708dde77131ab8d6d9ba2589ebb8686
blue2233 <nnurge@gmail.com>
2026-05-28 13:05:23 -0500

    created header and footer partials - created views folder - created header.ejs - created footer.ejs - add EJS include statement <%- include('header') %> - moved the header + footer code to the new ejs pages

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ dotenv.config();
 
 const app = express()
 const port = 3000
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static('public'));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+app.get("/index", (req, res) => {
+  res.render("index.ejs");
+});
 
 app.post("/mail", async (req, res) => {
   await utils
