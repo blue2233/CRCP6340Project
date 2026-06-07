1a2491cb8205fb499846aa8ba8f8966e2bccab6b
blue2233 <nnurge@gmail.com>
2026-06-06 21:28:06 -0500

    fix broken project images on the project detail page - the image src had a bad path format that caused images to load correctly from the home page but break when viewed from /project/:id

--- NicDev.session.sql
+++ NicDev.session.sql
@@ -1,1 +1,3 @@
-select * from projects
+UPDATE projects SET img_url = '/images/satellite.jpg' WHERE id = 1;
+UPDATE projects SET img_url = '/images/diffReact.png' WHERE id = 2;
+UPDATE projects SET img_url = '/images/purpleOrangeWidget.png' WHERE id = 3;
