093d503b9ac6b7094f6445137f8ee67272c5a2da
blue2233 <nnurge@gmail.com>
2026-06-22 23:03:50 -0500

    Add missing cors import and update port to 8080

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import * as utils from "./utils/utils.js";
 dotenv.config();
@@ -6,7 +7,7 @@ import * as db from "./utils/database.js";
 
 const app = express();
 app.use(cors());
-const port = 3000;
+const port = 8080;
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
