ee640fe090e8e3e5c0bbe5b8118c5f95126d31fe
blue2233 <nnurge@gmail.com>
2026-06-22 23:27:24 -0500

    Switch DB connection to object config with SSL cert and port 25060 for DigitalOcean

--- utils/database.js
+++ utils/database.js
@@ -1,31 +1,27 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 dotenv.config();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 let pool;
 
 export async function connect() {
-  let cString =
-    "mysql://" +
-    process.env.MYSQL_USER +
-    ":" +
-    process.env.MYSQL_PASSWORD +
-    "@" +
-    process.env.MYSQL_HOST +
-    ":" +
-    process.env.MYSQL_PORT +
-    "/" +
-    process.env.MYSQL_DATABASE;
   pool = mysql
-    .createPool(
-      cString  //digital ocean sql server
-      // {
-      //   host: process.env.MYSQL_HOST,
-      //   user: process.env.MYSQL_USER,
-      //   password: process.env.MYSQL_PASSWORD,
-      //   database: process.env.MYSQL_DATABASE,
-      // }
-    )
+    .createPool({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      port: process.env.MYSQL_PORT,
+      ssl: {
+        ca: readFileSync(join(__dirname, "../ca-certificate.crt")),
+        rejectUnauthorized: true,
+      },
+    })
     .promise();
 }
 
